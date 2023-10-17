5831cda072db5e68ea2d06edeef8c6cc78492674
alexissolis02 <142378965+alexissolis02@users.noreply.github.com>
2023-10-17 10:07:43 -0600

    Initial commit

---
+++ consulta.py
@@ -0,0 +1,18 @@
+#!C:/Python311/python.exe
+import mysql.connector
+import os
+import cgi
+import cgitb
+cgitb.enable()
+print("Content-type: text/html")
+print()
+print("<h1>Consulta Usuarios</h1>")
+print("<hr>")
+
+con = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='foro')
+cur = con.cursor()
+sql = "SELECT * FROM users"
+cur.execute(sql)
+for(email, password, name, avatar, role) in cur:
+    print("{}, {}, {}, {}, {}".format(email, password, name, avatar, role))
+    print("<br>")
